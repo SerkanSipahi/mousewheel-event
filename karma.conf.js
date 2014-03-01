6c4c16d9cfc9c857d62b6cf920329171c6b69d5f
Serkan Sipahi <serkan.sipahi@yahoo.de>
2014-03-01 22:04:25 +0100

    add yui node-event-simulate libs

--- karma.conf.js
+++ karma.conf.js
@@ -7,8 +7,36 @@ module.exports = function(config) {
         files: [
             'libs/classy.js',
             'mousewheel.js',
+
+            'yui3/build/yui/yui-min.js',
+
+            'yui3/build/oop/oop-min.js',
+            'yui3/build/event-custom-base/event-custom-base-min.js',
+            'yui3/build/event-base/event-base-min.js',
+            'yui3/build/dom-core/dom-core-min.js',
+            'yui3/build/dom-base/dom-base-min.js',
+            'yui3/build/selector-native/selector-native-min.js',
+            'yui3/build/selector/selector-min.js',
+            'yui3/build/node-core/node-core-min.js',
+            'yui3/build/color-base/color-base-min.js',
+            'yui3/build/dom-style/dom-style-min.js',
+            'yui3/build/node-base/node-base-min.js',
+
+            'yui3/build/gesture-simulate/gesture-simulate-min.js',
+            'yui3/build/node-event-simulate/node-event-simulate-min.js',
+            'yui3/build/event-custom-complex/event-custom-complex-min.js',
+            'yui3/build/async-queue/async-queue-min.js',
+            'yui3/build/dom-screen/dom-screen-min.js',
+            'yui3/build/node-screen/node-screen-min.js',
+            'yui3/build/gesture-simulate/gesture-simulate-min.js',
+            'yui3/build/event-custom-complex/event-custom-complex-min.js',
+
+            'yui3/build/event-simulate/event-simulate-min.js',
+
+            'tests/HTMLFixture.js',
             'tests/*.js'
         ],
+        basePath: './',
 		frameworks: ['jasmine'],
 		browser : ['Chrome', 'Firefox', 'Safari', 'Opera', 'IE', 'PhantomJS'],
 		exclude : [],
@@ -25,4 +53,5 @@ module.exports = function(config) {
 		singleRun : false,
 		reportSlowerThan : 500
 	});
+
 };

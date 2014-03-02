b032b2694e030efc898ac89c73a75ff3524f1b4a
Serkan Sipahi <serkan.sipahi@yahoo.de>
2014-03-02 22:00:33 +0100

    expanded to include more container

--- tests/HTMLFixture.js
+++ tests/HTMLFixture.js
@@ -3,15 +3,28 @@
  */
 function setUpHTMLFixture(){
 
-    console.log('setUpHTMLFixture...');
-
     var d              = document,
         bodyNode       = d.getElementsByTagName('body')[0],
         testDivFixture = d.createElement('div');
 
     testDivFixture.id = 'test-id';
     testDivFixture.className = 'test-class';
-    testDivFixture.innerHTML = 'Hello World!';
+    testDivFixture.innerHTML =
+
+    '<div class="main-outer-wrapper">'+
+        '<div id="some-id">Im id</div>'+
+        '<div class="other-class">Im class</div>'+
+        '<div class="some-class" data-test="foo">Im class with attribute [data-test="foo"]</div>'+
+        '<div id="some-class" data-test="foo">Im id with attribute [data-test="foo"]</div>'+
+        '<div id="some-class-2" data-test-1="foo" data-test-2="boo">Im id with attribute 2 x [data-test="foo"]</div>'+
+        '<div class="some-wrapper-class" data-wrapper="true">'+
+            '<div id="inner-wrapper">'+
+                '<div class="some-class" data-kind="liya">im liya</div>'+
+                '<div id="some-class-5" data-vater="serkan">im serkan</div>'+
+                '<div class="some-class-3" data-mutter="serkan">im goekcen</div>'+
+            '</div>'+
+        '</div>'+
+    '</div>';
 
     bodyNode.appendChild(testDivFixture);
 

f5af66fa3b027270655b2ebfc73c1c32893c1277
Serkan Sipahi <serkan.sipahi@yahoo.de>
2014-03-02 22:00:50 +0100

    - add crossbrowser selector match Testing for (ie8,9,10,11)! Success - add crossbrowser eventHandler Testing for(ie8,9,10,11)! Success - implement yui Simulating DOM Events

--- tests/mousewheel_tests.js
+++ tests/mousewheel_tests.js
@@ -1,7 +1,4 @@
-
-setUpHTMLFixture();
-
-describe("Type Testing:", function() {
+describe("Initialize helper functions...", function() {
 
     //Fixtures
     var instance        = {},
@@ -11,64 +8,94 @@ describe("Type Testing:", function() {
         StringFixture   = '',
         NULLFixture     = null;
 
+    setUpHTMLFixture();
+
     beforeEach(function() {
-        //init
-        instance = new Mousewheel('#test-id', function(e){
+        instance = new Mousewheel('#test-id', function(e){});
+    });
+
+    describe('Type Testing:', function(){
+
+        it("is an Object", function() {
+            expect(instance._is('Object', ObjectFixture)).toBe(true);
+        });
+        it("is a Function", function() {
+            expect(instance._is('Function', FunctionFixture)).toBe(true);
+        });
+        it("is an Array", function() {
+            expect(instance._is('Array', ArrayFixture)).toBe(true);
+        });
+        it("is an String", function() {
+            expect(instance._is('String', StringFixture)).toBe(true);
+        });
+        // > not working on IE8
+        it("is a Null", function() {
+            expect(instance._is('Null', NULLFixture)).toBe(true);
+        });
+
+
+    });
+
+    //works fine with ie8,9,10,11
+    describe("selector match Testing:", function() {
+
+        var result = null;
+
+        it("body tag", function() {
+            result = instance._$('body')[0].innerHTML;
+            expect(result).not.toBeUndefined();
+            expect(result).toMatch(/im liya/);
+        });
+        it("class", function() {
+            result = instance._$('.other-class')[0].innerHTML;
+            expect(result).not.toBeUndefined();
+            expect(result).toMatch(/Im class/);
+        });
+        it("class with dataset attribute", function() {
+            result = instance._$('.some-class[data-test="foo"]')[0].innerHTML;
+            expect(result).not.toBeUndefined();
+            expect(result).toMatch(/Im class with attribute \[data-test="foo"\]/);
+        });
+        it("id 2 x datasets", function() {
+            result = instance._$('#some-class-2[data-test-1="foo"][data-test-2="boo"]')[0].innerHTML;
+            expect(result).not.toBeUndefined();
+            expect(result).toMatch(/Im id with attribute 2 x \[data-test="foo"\]/);
+        });
+        it("simple nested selector query", function() {
+            result = instance._$('.main-outer-wrapper .some-wrapper-class #some-class-5')[0].innerHTML;
+            expect(result).not.toBeUndefined();
+            expect(result).toMatch(/im serkan/);
+        });
+        it("simple nested selector query + datasets attributes", function() {
+            result = instance._$('.main-outer-wrapper .some-wrapper-class[data-wrapper="true"] .some-class-3')[0].innerHTML;
+            expect(result).not.toBeUndefined();
+            expect(result).toMatch(/im goekcen/);
+        });
+
+        it("multiple selector matching", function() {
+            result = instance._$('#some-id, .other-class, #some-class-5');
+            expect(3).toEqual(result.length);
+        });
+
+    });
+
+    describe("eventHandler Testing:", function() {
+
+        it("click test", function() {
+
+            instance._$('#some-id')[0].on('click', function(e){
+                console.log('Hello World');
+            });
+
+            // > YUI click not working on IE8
+            // > but tested manuell on index.html and its work!
+
+            YUI().use('node-event-simulate', function(Y) {
+                Y.one("#some-id").simulate("click");
+            });
 
         });
     });
-    afterEach(function() {
-        //destroy
-        instance = {};
-    });
 
-    it("is an Object", function() {
-        expect(instance._is('Object', ObjectFixture)).toBe(true);
-    });
-    it("is a Function", function() {
-        expect(instance._is('Function', FunctionFixture)).toBe(true);
-    });
-    it("is an Array", function() {
-        expect(instance._is('Array', ArrayFixture)).toBe(true);
-    });
-    it("is an String", function() {
-        expect(instance._is('String', StringFixture)).toBe(true);
-    });
-    // > not working on IE8
-    it("is a Null", function() {
-        expect(instance._is('Null', NULLFixture)).toBe(true);
-    });
-
-});
-
-describe("selector match Testing:", function() {
-
-
-    //testen
-
-    // > instance._$('body')
-    // > instance._$('some-id')
-    // > instance._$('some-class')
-    // > instance._$('body[id="foo"]')
-
-});
-
-
-describe("eventHandler Testing:", function() {
-
-    /*
-    var t = document.getElementById('test-id');
-    t.addEventListener('click', function(){
-       console.log('im clicked');
-    });
-    */
-
-    YUI().use('node-event-simulate', function(Y) {
-        Y.one("#test-id").simulate("click");
-    });
-
-    it("is an Object", function() {
-        expect(true).toBe(true);
-    });
 });
 
